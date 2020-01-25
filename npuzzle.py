23da0f58c34ac73b51542f51e18575e5daf83bb7
Oleh SLOBODANIUK <osloboda@e1r4p6.unit.ua>
2020-01-25 19:51:08 +0200

    Update class State, add argparser

--- npuzzle.py
+++ npuzzle.py
@@ -1,27 +1,28 @@
 import os
 import sys
+import argparse
 
-
-def move(x, y, state):
-    for row in state:
-        for col in row:
-            if col == 0:
-                prev = state[y][x]
-                state[y][x] = col
-                col = prev
 
 class State:
-    def __init__(self, both, kids):
+    def __init__(self, both, kids, bones):
         self.both = both
-        self.kids = self.val = kids
+        self.kids = kids
+        self.bones = bones
+        self.theta = 0
+
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not os.path.exists(sys.argv[1]):
-        print("Usage: python3 npuzzle.py map.txt")
+
+    parser = argparse.ArgumentParser(description='Get arguments')
+    parser.add_argument('-f', action='store', dest='f', help='Input file name')
+    args = parser.parse_args()
+
+    if not args.f or not os.path.exists(args.f):
+        print("usage: npuzzle.py [-h] [-f] \n positional arguments:  -f     Input file name \n\noptional arguments: -h for help")
         exit()
     data = []
-    with open(sys.argv[1]) as f:
+    with open(args.f) as f:
         for line in f:
             subdata = []
             for x in line.split():
